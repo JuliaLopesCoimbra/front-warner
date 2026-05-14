bdbd343999b7b707550c50bd179444fa0a4a22d6
Julia Cristina Lopes Coimbra <114704249+JuliaLopesCoimbra@users.noreply.github.com>
2026-05-14 11:57:21 -0300

    docker

--- src/app/totem/cadastro/page.tsx
+++ src/app/totem/cadastro/page.tsx
@@ -1,10 +1,14 @@
+import { Suspense } from "react";
+
 import { CadastroScreen } from "@/components/totem/CadastroScreen";
 import { TotemStage } from "@/components/totem/TotemCanvas";
 
 export default function CadastroPage() {
   return (
     <TotemStage>
-      <CadastroScreen />
+      <Suspense>
+        <CadastroScreen />
+      </Suspense>
     </TotemStage>
   );
 }
